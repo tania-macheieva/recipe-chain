a60cbf8f843154c7c78323041525c76ea83cb32d
tania-macheieva <macheieva.tetiana@gmail.com>
2025-11-29 13:58:01 +0200

    Added default listening port

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const recipeRoutes = require('./routes/recipes');
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
